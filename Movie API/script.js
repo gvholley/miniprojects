04393286abb158a52169e88d3f565937c5f0aeda
Grant Holley <gvholley@gmail.com>
2021-02-26 21:21:02 +0900

    Movies display

--- Movie API/script.js
+++ Movie API/script.js
@@ -2,6 +2,7 @@ const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API ='https://api.themoviedb.org/3/search/movie?api_key=d5e9c2b90dbcc52ff15c63a729da2061&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -10,7 +11,34 @@ getMovies(API_URL)
 async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
-  console.log(data.results)
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHTML = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+    <div class="movie">
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+        <h3>Movie Title</h3>
+        <span class="${getClassByRating(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+    </div>
+    `
+
+    main.appendChild(movieEl)
+  })
 }
 
 form.addEventListener('submit', (e) => {
@@ -26,4 +54,14 @@ form.addEventListener('submit', (e) => {
     window.location.reload()
   }
 })
+
+function getClassByRating(vote) {
+  if(vote >= 8)  {
+    return 'green'
+  } else if(vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
 
