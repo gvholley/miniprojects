e5d536fdf3cc383636e67bd38f794f633c1a2e49
Grant Holley <gvholley@gmail.com>
2021-03-07 23:12:03 +0900

    To Do List WIP

---
+++ To Do List/script.js
@@ -0,0 +1,57 @@
+// Creates close button and appends it to each item
+var liEl = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < liEl.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "close";
+  //append button
+  span.appendChild(txt);
+  liEl[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a check when clicking on an item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the Add button
+function addToList() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please input a value");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
